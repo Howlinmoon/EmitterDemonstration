2e031da238185c3eaa4ed9a38e963bfa0026cb03
Jim Veneskey <github@strifecrafter.com>
2015-06-29 12:20:42 -0400

    initial commit of a new tutorial / project

---
+++ EmitterDemonstration/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  EmitterDemonstration
+//
+
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ EmitterDemonstrationTests/EmitterDemonstrationTests.m
@@ -0,0 +1,34 @@
+//
+//  EmitterDemonstrationTests.m
+//  EmitterDemonstrationTests
+//
+//  Created by jim Veneskey on 6/29/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EmitterDemonstrationTests : XCTestCase
+
+@end
+
+@implementation EmitterDemonstrationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
